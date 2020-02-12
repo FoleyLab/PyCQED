1139a97f993e4449adabdcb4f3705e5b2eabd6f4
FoleyLab <jayfoleyiv@gmail.com>
2020-02-12 06:55:33 -0500

    hopefully fixed initial condition in outter loop

--- pol_sys_input.py
+++ pol_sys_input.py
@@ -90,18 +90,23 @@ for j in range(0,N_repeats):
     vi_init = polt.V
     
     ### uncomment if you wish to print the initial conditions
-    #print("  Initial Position is ",polt.R)
-    #print("  Initial Velocity is ",polt.V)
+    print("  Initial Position is ",polt.R)
+    print("  Initial Velocity is ",polt.V)
     
     ### Build local and polariton matrices!
     polt.H_e()
     polt.H_total = np.copy(polt.H_electronic + polt.H_photonic + polt.H_interaction)
     polt.Transform_L_to_P()
     polt.D_polariton = np.zeros((polt.N_basis_states,polt.N_basis_states),dtype=complex)
-    polt.D_polariton[polt.initial_state,polt.initial_state] = 1+0j
+    ### hard-coding this!
+    polt.D_polariton[2,2] = 1+0j
     polt.D_local = np.outer(polt.transformation_vecs_L_to_P[:,polt.initial_state], np.conj(polt.transformation_vecs_L_to_P[:,polt.initial_state])) 
     polt.Energy = polt.TrHD(polt.H_total, polt.D_local)
     polt.Transform_L_to_P()
+    print("Polariton")
+    print(D_polariton)
+    print("Local")
+    print(D_local)
     
     ### un-comment open files for writing data about electronic and nuclear dynamics
     '''
